d008e73d1f9832c10920776e886aa4feda615ebd
Cole Mellem <colechristianmellem@gmail.com>
2026-02-24 01:31:15 +0200

    Created data retrieval script for nycflights13

---
+++ scripts/01_retrieve_data.R
@@ -0,0 +1,15 @@
+install.packages("tidyverse")
+install.packages("nycflights13")
+
+library(tidyverse)
+
+library(nycflights13)
+
+write_csv(flights, "data/raw/flights.csv")
+
+write_csv(airlines, "data/raw/airlines.csv")
+
+write_csv(weather, "data/raw/weather.csv")
+
+write_csv(airports, "data/raw/flights.csv")
+
