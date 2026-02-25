23f6be000a0fb9a9c2eca67ae03abd8a8b241077
Cole Mellem <colechristianmellem@gmail.com>
2026-02-26 01:51:58 +0200

    Cleaned, filtered, NA handling, duplicates removed from flights. Weekday and month features generated. Saved as parquet

--- scripts/02_data_cleaning.R
+++ scripts/02_data_cleaning.R
@@ -5,18 +5,64 @@ library(tidyverse)
 library(arrow)
 library(janitor)
 
+
+# reading data
+
 flights_raw <- read_csv("data/raw/flights.csv")
-weather_raw <- read_csv("data/raw/weather.csv")
-airlines_raw <- read_csv("data/raw/airlines.csv")
-airports_raw <- read_csv("data/raw/airports.csv")
 
-head(flights_raw)
-glimpse(flights_raw)
-summary(flights_raw)
+airlines_raw <- read_csv("data/raw/airlines.csv") |>
+  rename(airline_name = name)
 
-head(weather_raw)
-summary(weather_raw)
+airports_raw <- read_csv("data/raw/airports.csv") |>
+  rename(airport_name = name)
 
-head(airlines_raw)
+airports_raw_filtered <- airports_raw[ ,c("faa","airport_name")] 
 
-head(airports_raw)
+# joining data
+
+flights_raw_filtered <- flights_raw[ ,c("flight","carrier","origin","dest","time_hour","dep_delay","arr_delay")] |>
+  mutate(flight_id = row_number()) |>
+  left_join(airlines_raw, by = "carrier") |>
+  left_join(airports_raw_filtered, by = join_by("origin" == "faa")) |>
+  rename(airport_origin_name = airport_name) |>
+  left_join(airports_raw_filtered, by = join_by("dest" == "faa")) |>
+  rename(airport_destination_name = airport_name)
+
+# Each row = one scheduled commercial departure from NYC 
+
+# generating month, day, renaming 
+
+flights_joined <- flights_raw_filtered[ , c("flight_id", "flight", "airline_name","airport_origin_name","airport_destination_name","dep_delay","arr_delay","time_hour")] |>
+  mutate(day_of_week = wday(time_hour, label = TRUE, abbr = FALSE)) |>
+  mutate(month = month(time_hour, label = TRUE, abbr = FALSE)) |>
+  rename(date_time = time_hour) |>
+  mutate(arrival_delay = case_when(
+    arr_delay > 60 ~ "Significant",
+    arr_delay > 15 ~ "High",
+    arr_delay > 0 ~ "Low" , 
+    TRUE ~ "None"
+     )) |>
+  mutate(departure_delay = case_when(
+    dep_delay > 60 ~ "Significant",
+    dep_delay > 15 ~ "High",
+    dep_delay > 0 ~ "Low",
+    TRUE ~ "None"
+  ))
+
+# removing invalid flights and duplicates
+
+flights_clean <- flights_joined |>
+  filter(!is.na(dep_delay)) |>
+  filter(!is.na(arr_delay)) |>
+  filter(!is.na(airport_destination_name)) |>
+  filter(!is.na(airport_origin_name)) |>
+  filter(!is.na(airline_name)) |>
+  distinct(airline_name, flight, airport_origin_name, airport_destination_name, dep_delay, arr_delay, date_time, .keep_all = TRUE) |>
+  filter(year(date_time) == 2013)
+
+summary(flights_clean)
+glimpse(flights_clean)
+
+# save data as parquet
+
+write_parquet(flights_clean, "data/clean/flights_cleaned.parquet")
